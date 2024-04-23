8635618b4f7534838d05a4363348943fcb9abf33
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2024-04-23 12:08:22 +0200

    Add empty pages and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./components/ui/Navbar";
+import Stories from "./pages/Stories";
+import Features from "./pages/Features";
+import Pricing from "./pages/Pricing";
 
 function App() {
   return (
     <>
+      <header>
+        <Navbar />
+      </header>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/stories" element={<Stories />} />
+        <Route path="/features" element={<Features />} />
+        <Route path="/pricing" element={<Pricing />} />
       </Routes>
     </>
   );
